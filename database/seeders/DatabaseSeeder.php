c4a40f67074715dad1ac3e23032ea82491ba02e5
RayonRafael <rafaelrayon530@gmail.com>
2026-08-25 13:10:18 -0600

    fix: railway.toml, Dockerfile y DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,35 +10,29 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        $this->call([
-            RolesAndPermissionsSeeder::class,
-            AdminUserSeeder::class,
-            ServicioSocialUserSeeder::class,
-        ]);
-        $this->call(AreaTableSeeder::class);
-        $this->call(BitacoraSistemaTableSeeder::class);
+        $this->call(RolesAndPermissionsSeeder::class);
+        $this->call(AdminUserSeeder::class);
+        $this->call(ServicioSocialUserSeeder::class);
         $this->call(DepartamentoTableSeeder::class);
-        $this->call(DetalleSolicitudTableSeeder::class);
-        $this->call(HistorialEstadosTableSeeder::class);
-        $this->call(InventarioTableSeeder::class);
-        $this->call(MantenimientoTableSeeder::class);
+        $this->call(AreaTableSeeder::class);
+        $this->call(TipoMaterialTableSeeder::class);
         $this->call(MarcaMaterialTableSeeder::class);
+        $this->call(UnidadMedidaTableSeeder::class);
         $this->call(MaterialTableSeeder::class);
-        $this->call(ModelHasPermissionsTableSeeder::class);
-        $this->call(ModelHasRolesTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
         $this->call(ProveedoresTableSeeder::class);
         $this->call(ReceptorTableSeeder::class);
-        $this->call(RoleHasPermissionsTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
+        $this->call(InventarioTableSeeder::class);
         $this->call(SolicitudTableSeeder::class);
-        $this->call(TipoMaterialTableSeeder::class);
-        $this->call(UnidadMedidaTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
+        $this->call(DetalleSolicitudTableSeeder::class);
+        $this->call(MantenimientoTableSeeder::class);
+        $this->call(HistorialEstadosTableSeeder::class);
+        $this->call(BitacoraSistemaTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(ModelHasPermissionsTableSeeder::class);
+        $this->call(ModelHasRolesTableSeeder::class);
+        $this->call(RoleHasPermissionsTableSeeder::class);
     }
 }
